cef5d659a0677f4e43a2cb40b1e445f3fb9a428f
Jester <ABooth9422@gmail.com>
2019-08-01 19:33:47 -0400

    test

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,5 +1,5 @@
 var expect = require("chai").expect;
-
+var describe = require("chai").describe;
 describe("canary test", function() {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
