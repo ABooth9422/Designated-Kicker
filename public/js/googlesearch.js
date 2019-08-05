7ea54994cffd39cf763cdddff88b44cc1bbd32a9
Jester <ABooth9422@gmail.com>
2019-08-05 17:13:25 -0400

    google search  completed items appended to page

--- public/js/googlesearch.js
+++ public/js/googlesearch.js
@@ -8,9 +8,9 @@ $("#startSearch").on("click", function() {
 
 function getSearchResults(categoryInput, keywordInput) {
   var queryURL =
-    "https://www.googleapis.com/customsearch/v1?key=AIzaSyD-_9weq6nvkj3TD9WBn1h1FaJJQbu-CII&cx=017576662512468239146:omuauf_lfve&q=" +
+    "https://www.googleapis.com/customsearch/v1?key=AIzaSyAWKVtPudQIR6t8cn8AxA_voG_IvPKfr20&cx=11624574961326826142:xd1gsah2vww&q=" +
     categoryInput +
-    " " +
+    "+" +
     keywordInput +
     "+kickstarter+projects";
 
@@ -19,10 +19,25 @@ function getSearchResults(categoryInput, keywordInput) {
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    $("#googleSearchOne").html(response.items[0].title);
-    $("#googleSearchTwo").html(response.items[1].title);
-    $("#googleSearchThree").html(response.items[2].title);
-    $("#googleSearchFour").html(response.items[3].title);
-    $("#googleSearchFive").html(response.items[4].title);
+    var resultArray = [];
+    for (var index = 0; index < 5; index++) {
+      resultArray.push(response.items[index]);
+    }
+    resultArray.forEach(function(element) {
+      var div = $("<div>");
+      div.addClass("row d-flex justify-content-center text-white my-3");
+      var image = $("<img src= still image'>");
+      image.attr("src", element.pagemap.cse_thumbnail[0].src);
+      image.addClass("img-thumbnail");
+      var content = $("<h5>");
+      content.text("Description: " + element.title);
+      var webLink = $("<a>");
+      webLink.text(element.link);
+      webLink.attr("href", element.link);
+      webLink.attr("target", "_blank");
+      div.append(image, content, webLink);
+
+      div.appendTo($("#scrollContResults"));
+    });
   });
 }
