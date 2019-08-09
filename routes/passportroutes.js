739fd73cf494af8a848476a541e36e154fae0fde
Justin <justinconforti@gmail.com>
2019-08-09 09:09:34 -0400

    cleaned up Passport code

--- routes/passportroutes.js
+++ routes/passportroutes.js
@@ -3,19 +3,9 @@ var db = require("../models");
 var passport = require("../config/passport");
 //
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will happen on the front end
-    // They won't get this or even be able to access this page if they aren't authed
     res.json("/startSearch");
   });
-  //
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
   app.post("/api/signup", function(req, res) {
     console.log(req.body);
     db.User.create({
@@ -28,11 +18,8 @@ module.exports = function(app) {
       .catch(function(err) {
         console.log(err);
         res.json(err);
-        // res.status(422).json(err.errors[0].message);
       });
   });
-  //
-  // Route for logging user out
   app.post("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
