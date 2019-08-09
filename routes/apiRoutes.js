7beed6365c2b9eca2108c21310cee9c137f8baf0
Justin <justinconforti@gmail.com>
2019-08-08 21:31:12 -0400

    passport changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,19 +8,6 @@ module.exports = function(app) {
     console.log(req.body.cat);
     var cat = req.body.cat;
     console.log(cat);
-    // db.kickstarter
-    //   .findAll(
-    //     {
-    //       where: {
-    //         category: cat
-    //       }
-    //     },
-    //     { raw: true }
-    //   )
-    //   .then(function(data) {
-    //     console.log("callback data" + data);
-    //     resp.status(201).end();
-    //   });
   });
 
   app.post("/add", function(req, resp) {
@@ -39,26 +26,4 @@ module.exports = function(app) {
       console.log(data);
     });
   });
-
-  // Get all examples
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
-  //     dbExample
-  //   ) {
-  //     res.json(dbExample);
-  //   });
-  // });
 };
